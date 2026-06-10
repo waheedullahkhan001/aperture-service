41221cb8dcde02f6282b7dbbd9b01d76c5bc2299
Waheedullah Khan <waheedullahkhan1250@gmail.com>
2026-06-10 23:50:44 +0500

    chore: review fixes for build setup

--- src/test/java/com/aperture/apertureservice/support/IntegrationTest.java
+++ src/test/java/com/aperture/apertureservice/support/IntegrationTest.java
@@ -5,11 +5,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+@Documented
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @SpringBootTest
